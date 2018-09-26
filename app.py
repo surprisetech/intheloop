f0c7e3fec0a4ed15a7a5157e6a97b88ce24e8ad1
Brojowski <alex.gajowski@gmail.com>
2018-09-25 23:47:51 -0400

    Set up serving static files. Add an index page and a style page.

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, send_file
 import praw
 from config import RedditConfig
 
-app = Flask(__name__)
+# Send "public/any.name" when route "<site>.com/{any.name}" is hit
+app = Flask(__name__, static_url_path="", static_folder="public")
 
+# Initialize PRAW (reddit wrapper) from config.py
+# DO NOT push config.py to github, we do not want to
+# make the API keys public.
+# The format of config.py is:
+#
+# class RedditConfig:
+#     id = '<client_id>'
+#     secret = '<client_secret>'
+#     userAgent = '<user_agent>'
 reddit = praw.Reddit(client_id=RedditConfig.id,
                      client_secret=RedditConfig.secret,
                      user_agent=RedditConfig.userAgent)
 
+
+# Remap '/' to index. Other files can be served statically.
 @app.route('/')
 def index():
-    return 'The future home of InTheLoop'
-
+    return send_file('public/index.html')
