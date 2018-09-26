c5add60ec2f618992826f3535ae7028dad388623
Brojowski <alex.gajowski@gmail.com>
2018-09-25 23:06:55 -0400

    Initalize PRAW

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import praw
+from config import RedditConfig
 
 app = Flask(__name__)
+
+reddit = praw.Reddit(client_id=RedditConfig.id,
+                     client_secret=RedditConfig.secret,
+                     user_agent=RedditConfig.userAgent)
 
 @app.route('/')
 def index():
