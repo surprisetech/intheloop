f4d1a43364f422f6f83f46812ad5b06cc45cb6bc
Brojowski <alex.gajowski@gmail.com>
2018-10-24 20:35:26 -0400

    finish user text counting

--- app.py
+++ app.py
@@ -37,12 +37,15 @@ def topPostsAllTime(sr):
 	return reddit.subreddit(sr).top(time_filter='all', limit=100)
 	
 def topPostsPast24Hours(sr):
+	print("topPostsPast24Hours")
 	return reddit.subreddit(sr).top(time_filter='day', limit=100)	
 
 def controversalPostsAllTime(sr):
+	print("controversalPostsAllTime")
 	return reddit.subreddit(sr).controversial(time_filter = 'all', limit=100)
 	
 def controversalPast24Hours(sr):
+	print("controversalPast24Hours")
 	return reddit.subreddit(sr).controversial(time_filter = 'day', limit=100)
 
 
@@ -58,13 +61,12 @@ def wordCountSubreddit(sr, category):
 	submissions = switch.get(category)
 	posts = list(map(lambda x: x.selftext + " " + x.title, submissions))
 	sortedWords = countWords(posts, punctRm, excludeWordsList)
+	sortedWords = sortedWords[:50]
 	labels = list()
 	values = list()
 	for word in sortedWords:
 		labels.append(word[0])
 		values.append(word[1])
-	labels = labels[:50]
-	values = values[:50]
 
 	# Generate chart.
 	fig = plt.figure()
@@ -75,9 +77,30 @@ def wordCountSubreddit(sr, category):
 #word popularity by user-KT
 @app.route('/count/<user>/')
 def wordCountUser(user):
-	userOutput
-	user = list(map(lambda x: x.name + " " + x.word, userOutput ))
-	return json.jsonify(user)
+	user = reddit.redditor(name=user)
+	comments = user.comments.hot(limit=100)
+	submissions = user.submissions.hot(limit=100)
+
+	usersText = list()
+	for comment in comments:
+		usersText.append(comment.body)
+
+	for sub in submissions:
+		usersText.append(sub.selftext)
+
+	sortedWords = countWords(usersText, punctRm, excludeWordsList)
+	sortedWords = sortedWords[:50]
+	labels = list()
+	values = list()
+	for word in sortedWords:
+		labels.append(word[0])
+		values.append(word[1])
+
+	# Generate chart.
+	fig = plt.figure()
+	plt.bar(range(len(labels)), values, tick_label=labels)
+
+	return mpld3.fig_to_html(fig)
 
 
 punctRm = str.maketrans('', '', string.punctuation + "“”’")
